641a7e2e6c522100e4a8f72bb092a3f5c875c22a
Martin Frantisak <martin.frantisak@gmail.com>
2022-11-06 14:23:27 +0100

    LA - removed navigation bar, added pendings component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './components/main-page/catalog/catalog.component';
+import { PendingsComponent } from './components/main-page/pendings/pendings.component';
 import { UsersComponent } from './components/main-page/users/users.component';
 
 const routes: Routes = [
   { path: '', component: CatalogComponent },
   { path: 'catalog', component: CatalogComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'pendings', component: PendingsComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CatalogComponent } from './components/main-page/catalog/catalog.component';
 import { UsersComponent } from './components/main-page/users/users.component';
 import { SidebarItemComponent } from './components/sidebar/sidebar-item/sidebar-item.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PendingsComponent } from './components/main-page/pendings/pendings.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationBarComponent, SidebarComponent, CatalogComponent, UsersComponent, SidebarItemComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    CatalogComponent,
+    UsersComponent,
+    SidebarItemComponent,
+    SearchBarComponent,
+    PendingsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
