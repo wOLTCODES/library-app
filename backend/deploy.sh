e63f280049a5ea82751f8b98b8c7275e2b172b5b
wOLTCODES <executedwell@gmail.com>
2022-11-18 17:06:26 +0100

    Fix deployment

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-scp target/library-app-0.0.1-SNAPSHOT.jar root@likandros.cz:/var/www/html/woltcodes.com/knihovna/
+scp target/library-app-0.0.1-SNAPSHOT.jar $1:/var/www/html/woltcodes.com/knihovna/
+PID=$(ssh $1 ps aux | grep 'library-app-0.0.1-SNAPSHOT.jar' | head -n1 | awk '{ print $2 }')
+echo $PID
+ssh $1 kill -n 9 $PID
+ssh $1 "screen -S knihovna -X stuff 'bash /var/www/html/woltcodes.com/knihovna/start.sh \015'"
