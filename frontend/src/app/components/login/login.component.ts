b5d50931c2e4000967e003d88b9e774719495a95
Martin Frantisak <martin.frantisak@gmail.com>
2022-11-18 14:03:59 +0100

    login refactor

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,18 @@
-import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import {
   HttpClient,
   HttpErrorResponse,
   HttpHeaders,
 } from '@angular/common/http';
-import {Router} from "@angular/router";
-import {UserServiceService} from "../../services/user-service.service";
+import { Router } from '@angular/router';
+import { UserServiceService } from '../../services/user-service.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-login',
@@ -16,9 +23,14 @@ export class LoginComponent implements OnInit {
   @ViewChild('username') username: ElementRef;
   @ViewChild('password') password: ElementRef;
 
-  private _messageIdIterator = 0
+  private _messageIdIterator = 0;
 
-  constructor(private _http: HttpClient, private _router: Router, private _renderer: Renderer2, private _element:ElementRef, private _userService: UserServiceService) {}
+  constructor(
+    private _http: HttpClient,
+    private _router: Router,
+    private _userService: UserServiceService,
+    private _messageS: MessageService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -28,29 +40,18 @@ export class LoginComponent implements OnInit {
     formData.append('password', this.password.nativeElement.value);
 
     this._http
-      .post('/knihovna/api/user/login', formData, {observe: 'response'})
+      .post('/knihovna/api/user/login', formData, { observe: 'response' })
       .subscribe({
         next: (response) => {
-          this._userService.updateUser()
-          this._router.navigate(['catalog'])
+          this._userService.updateUser();
+          this._router.navigate(['catalog']);
         },
         error: (error: any) => {
           if (error.status === 401) {
-            const errorMessageContainer = this._renderer.createElement('div')
-            const id = this._messageIdIterator++;
-            this._renderer.setAttribute(errorMessageContainer, 'id', id.toString())
-            this._renderer.addClass(errorMessageContainer, 'error-message')
-            this._renderer.setProperty(errorMessageContainer, 'innerHTML', error.error.message)
-            this._renderer.appendChild(this._element.nativeElement, errorMessageContainer)
-            setTimeout(() => {
-              document.getElementById(id.toString())?.classList.add('show')
-            }, 100)
-            setTimeout(() => {
-              document.getElementById(id.toString())?.classList.remove('show')
-            }, 2500)
-            setTimeout(() => {
-              document.getElementById(id.toString())?.remove()
-            }, 2700)
+            this._messageS.generateMessage(
+              error.error.message,
+              'error-message'
+            );
           }
         },
       });
