75ee0462971b63ca73576adeec9fd6c1a6223ee1
Martin Frantisak <martin.frantisak@gmail.com>
2022-11-18 11:59:58 +0100

    updated users table

--- frontend/src/app/components/main-page/users/users.component.ts
+++ frontend/src/app/components/main-page/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {User} from "../../../model/User";
+import { HttpClient } from '@angular/common/http';
+import { User } from '../../../model/User';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -8,27 +9,29 @@ import {User} from "../../../model/User";
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-  public users: User[]
+  public users: User[];
+  public isLoaded = new BehaviorSubject<boolean>(false);
 
   constructor(private _http: HttpClient) {}
 
   ngOnInit(): void {
-    this.fetch()
+    this.fetch();
   }
 
   fetch() {
     //fetch all books
+    this.isLoaded.next(false);
+
     this._http
-      .get<User[]>('/knihovna/api/user', {observe: 'response'})
+      .get<User[]>('/knihovna/api/user', { observe: 'response' })
       .subscribe({
         next: (response) => {
-          let users = response.body
-          if (users == null) throw new Error('No body')
-          this.users = users
+          let users = response.body;
+          if (users == null) throw new Error('No body');
+          this.users = users;
+          this.isLoaded.next(true);
         },
-        error: (error: any) => {
-
-        },
+        error: (error: any) => {},
       });
   }
 }
