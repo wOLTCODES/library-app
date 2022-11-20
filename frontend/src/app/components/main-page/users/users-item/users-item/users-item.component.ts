41d870f02aa98ea7517de53b99f1ac2ac50437d5
Martin Frantisak <martin.frantisak@gmail.com>
2022-11-20 17:32:08 +0100

    users actions implemented

--- frontend/src/app/components/main-page/users/users-item/users-item/users-item.component.ts
+++ frontend/src/app/components/main-page/users/users-item/users-item/users-item.component.ts
@@ -1,22 +1,83 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {User} from "../../../../../model/User";
-import {faDownLong, faHammer, faUpLong, faUserSlash} from "@fortawesome/free-solid-svg-icons";
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../../../../model/User';
+import {
+  faDownLong,
+  faHammer,
+  faUpLong,
+  faUserSlash,
+} from '@fortawesome/free-solid-svg-icons';
+import { UserService } from 'src/app/services/user.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users-item',
   templateUrl: './users-item.component.html',
-  styleUrls: ['./users-item.component.scss']
+  styleUrls: ['./users-item.component.scss'],
 })
 export class UsersItemComponent implements OnInit {
   @Input() user: User;
-  faUp = faUpLong
-  faDown = faDownLong
-  faBan = faHammer
-  faUnban = faUserSlash
+  faUp = faUpLong;
+  faDown = faDownLong;
+  faBan = faHammer;
+  faUnban = faUserSlash;
 
-  constructor() { }
+  constructor(private _userS: UserService, private _http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public promote(): void {
+    this._http
+      .post<User[]>(`/knihovna/api/user/promote/${this.user.id}`, {
+        observe: 'response',
+      })
+      .subscribe({
+        next: () => {
+          this._userS.updateUser();
+          this._userS.fetch();
+        },
+        error: (error: any) => {},
+      });
   }
 
+  public demote(): void {
+    this._http
+      .post<User[]>(`/knihovna/api/user/demote/${this.user.id}`, {
+        observe: 'response',
+      })
+      .subscribe({
+        next: () => {
+          this._userS.updateUser();
+          this._userS.fetch();
+        },
+        error: (error: any) => {},
+      });
+  }
+
+  public ban(): void {
+    this._http
+      .post<User[]>(`/knihovna/api/user/ban/${this.user.id}`, {
+        observe: 'response',
+      })
+      .subscribe({
+        next: () => {
+          this._userS.updateUser();
+          this._userS.fetch();
+        },
+        error: (error: any) => {},
+      });
+  }
+
+  public unban(): void {
+    this._http
+      .post<User[]>(`/knihovna/api/user/unban/${this.user.id}`, {
+        observe: 'response',
+      })
+      .subscribe({
+        next: () => {
+          this._userS.updateUser();
+          this._userS.fetch();
+        },
+        error: (error: any) => {},
+      });
+  }
 }
