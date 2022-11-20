54b17ba00a8dd5ef8bdfea1d24d85d944491cf88
Martin Frantisak <martin.frantisak@gmail.com>
2022-11-20 15:14:00 +0100

    pendings lazyload

--- frontend/src/app/components/main-page/pendings/pendings.component.ts
+++ frontend/src/app/components/main-page/pendings/pendings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../../model/User';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-pendings',
@@ -9,6 +10,7 @@ import { User } from '../../../model/User';
 })
 export class PendingsComponent implements OnInit {
   public users: User[];
+  public isLoaded = new BehaviorSubject<boolean>(false);
 
   constructor(private _http: HttpClient) {}
 
@@ -17,16 +19,16 @@ export class PendingsComponent implements OnInit {
   }
 
   fetch() {
+    this.isLoaded.next(false);
     //fetch all books
     this._http
       .get<User[]>('/knihovna/api/user/pending', { observe: 'response' })
       .subscribe({
         next: (response) => {
-          console.log(response);
-
           let users = response.body;
           if (users == null) throw new Error('No body');
           this.users = users;
+          this.isLoaded.next(true);
         },
         error: (error: any) => {},
       });
