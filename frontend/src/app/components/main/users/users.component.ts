28a58b05c34837d3c159ee3af714ae68f6433291
Martin Frantisak <martin.frantisak@gmail.com>
2022-12-04 18:58:04 +0100

    catalog, users template and styles update

--- frontend/src/app/components/main/users/users.component.ts
+++ frontend/src/app/components/main/users/users.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../../../model/User';
 import { BehaviorSubject } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-users',
@@ -11,6 +12,8 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class UsersComponent implements OnInit {
   constructor(public userS: UserService) {}
+
+  faArrowDown = faArrowDown;
 
   ngOnInit(): void {
     this.userS.fetch();
